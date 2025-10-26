a80b230e2913e5c9da4a4a881050fef710350fde
LeoFernanndes <balbino.leonel@gmail.com>
2025-10-26 02:39:34 -0300

    feat: update endpoints and base implementations to comply with ruff's rules

--- src/langchain_impl/openai/simple_chat.py
+++ src/langchain_impl/openai/simple_chat.py
@@ -1,6 +1,7 @@
+"""Simple implementation of OpenAiMessages."""
+
 import os
 from pathlib import Path
-
 from typing import List, Literal, Union
 
 from dotenv import load_dotenv
@@ -9,11 +10,16 @@ from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, System
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
 
-
 load_dotenv()
 
 
 class OpenAiMessage(BaseModel):
+    """Message Body to be used on OpenAi Chats.
+    
+    Properties:
+        sender: Type of message sender.
+        content: Message content
+    """
     sender: Union[Literal['ai'], Literal['human'], Literal['system']]
     content: str
 
@@ -26,7 +32,14 @@ contexts_dir = os.path.join(parent_dir, 'contexts')
 
 
 def get_context(name: str) -> BaseMessage:
-
+    """Helper function to get the context persona by name.
+    
+    Params:
+        name: Context persona name.
+    
+    Retunrs:
+        Content persona content as a system message.
+    """
     available_contexts = {
         'high_school_counselor': 'counselor_context.md'
     }
@@ -39,23 +52,25 @@ def get_context(name: str) -> BaseMessage:
 
 
 class OpenAiSimpleChat:
+    """Simple OpenAi Chat."""
 
     def chat(self, messages: List[OpenAiMessage], context_name: str) -> BaseMessage:
-
+        """Returns a BaseMessage response from OpenAi.
+        
+        Params:
+            messages: List of OpenAiMessage.
+            context_name: Context persona.
+        """
         context = get_context(context_name)
-
         _messages: List[BaseMessage] = [context]
         for m in messages:
             if m.sender == 'ai':
                 _m = AIMessage(content=m.content)
             elif m.sender == 'human':
-                _m = AIMessage(content=m.content)
+                _m = HumanMessage(content=m.content)
             elif m.sender == 'system':
-                _m = AIMessage(content=m.content)
-
-            try:
-                _messages.append(_m)
-            except ValueError as ve:
-                print(ve)
-        print(_messages)
+                _m = SystemMessage(content=m.content)
+            else:
+                raise Exception("Unknown OpenAi message type")
+            _messages.append(_m)
         return chat.invoke(_messages)
